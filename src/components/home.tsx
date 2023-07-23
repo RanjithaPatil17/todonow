2efe6957ddb578ae8eea517eac0a05d69edecbc7
RanjithaPatil17 <ranjithapatil017@gmail.com>
2023-07-23 21:45:37 +0530

    change font size

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,7 +12,13 @@ export default class Home extends Component<Props, State> {
   render() {
     
     return (
-      <div className="col-md-12">
+        <div className="col-md-12"
+            style={
+                 { fontSize: '60px', // Adjust the font size as needed
+                    fontWeight: 'bold'
+                }
+            }
+        >
         TodoNow application
       </div>
     );
