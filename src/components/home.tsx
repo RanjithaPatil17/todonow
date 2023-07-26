25dca52fc989103f9722df1d189c794e1c88cac5
Ranjitha <ranjithapatil017@gmail.com>
2023-07-26 13:42:36 +0530

    add home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,24 +1,31 @@
 import { Component } from "react";
 
-
 type Props = {};
 
-type State = {
-
-};
+type State = {};
 
 export default class Home extends Component<Props, State> {
-
   render() {
-    
     return (
-        <div className="col-md-12"
-            style={
-                 { fontSize: '60px', // Adjust the font size as needed
-                    fontWeight: 'bold'
-                }
-            }
-        >
+      <div
+        className="col-md-12"
+        style={{
+          fontSize: '60px', // Adjust the font size as needed
+          fontWeight: 'bold',
+          fontFamily: 'Lugrazia, cursive', // Use "Lugrazia" font family
+          backgroundColor: '#f0f0f0',
+          color: '#333',
+          padding: '20px',
+          textAlign: 'center',
+          borderRadius: '10px',
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+          border: '2px solid #ccc', // Add a border around the div
+          maxWidth: '800px', // Limit the maximum width of the div
+          margin: '0 auto', // Center the div horizontally
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)', // Add a text shadow
+          lineHeight: '1.2', // Set the line height for better readability
+        }}
+      >
         TodoNow application
       </div>
     );
